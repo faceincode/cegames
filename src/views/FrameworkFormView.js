6d6a96c494b9777f10ba70c6e701c23551e0f988
faceincode <roberto@cegames.ca>
2020-11-18 16:44:47 -0800

    appfairy: Migrate

---
+++ src/views/FrameworkFormView.js
@@ -0,0 +1,83 @@
+/* eslint-disable */
+
+import React from 'react'
+import { createScope, map, transformProxies } from './helpers'
+
+const scripts = [
+
+]
+
+let Controller
+
+class FrameworkFormView extends React.Component {
+  static get Controller() {
+    if (Controller) return Controller
+
+    try {
+      Controller = require('../controllers/FrameworkFormController')
+      Controller = Controller.default || Controller
+
+      return Controller
+    }
+    catch (e) {
+      if (e.code == 'MODULE_NOT_FOUND') {
+        Controller = FrameworkFormView
+
+        return Controller
+      }
+
+      throw e
+    }
+  }
+
+  componentDidMount() {
+    scripts.concat(Promise.resolve()).reduce((loaded, loading) => {
+      return loaded.then((script) => {
+        new Function(`
+          with (this) {
+            eval(arguments[0])
+          }
+        `).call(window, script)
+
+        return loading
+      })
+    })
+  }
+
+  render() {
+    const proxies = Controller !== FrameworkFormView ? transformProxies(this.props.children) : {
+      'email': [],
+      'submit': [],
+    }
+
+    return (
+      <span>
+        <style dangerouslySetInnerHTML={{ __html: `
+          @import url(/css/normalize.css);
+          @import url(/css/webflow.css);
+          @import url(/css/celanding.webflow.css);
+        ` }} />
+        <span className="af-view">
+          <div>
+            <div className="af-class-form-block w-form">
+              <form id="email-form" name="email-form" data-name="Email Form" className="af-class-subscribe-form-flex">
+                <div className="af-class-subscribe-form-input-wrapper">{map(proxies['email'], props => <input type="email" maxLength={256} name="Subscriber-Email" data-name="Subscriber Email" placeholder="E-Mail Address" id="Subscriber-Email" required {...{...props, className: `af-class-subscribe-form-input w-input ${props.className || ''}`}}>{props.children}</input>)}</div>
+                <div className="af-class-div-block-5">{map(proxies['submit'], props => <input type="submit" value="Subscribe" data-wait="Please wait..." {...{...props, className: `af-class-submit-button-2 w-button ${props.className || ''}`}}>{props.children}</input>)}</div>
+              </form>
+              <div className="af-class-success-message w-form-done">
+                <div className="af-class-text-block">Thank you for subscribing.</div>
+              </div>
+              <div className="af-class-error-message af-class-error-message-2 w-form-fail">
+                <div className="af-class-text-block-2">Subscription&nbsp;Error.&nbsp;Please try again.</div>
+              </div>
+            </div>
+          </div>
+        </span>
+      </span>
+    )
+  }
+}
+
+export default FrameworkFormView
+
+/* eslint-enable */
--- src/views/IndexView.js
+++ src/views/IndexView.js
@@ -2,6 +2,7 @@
 
 import React from 'react'
 import { createScope, map, transformProxies } from './helpers'
+import FrameworkFormView from './FrameworkFormView'
 
 const scripts = [
   fetch("https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=5fb2ce675a0bc01f7fb15678").then(body => body.text()),
@@ -47,7 +48,7 @@ class IndexView extends React.Component {
 
   render() {
     const proxies = Controller !== IndexView ? transformProxies(this.props.children) : {
-
+      'framework-form': [],
     }
 
     return (
@@ -63,7 +64,7 @@ class IndexView extends React.Component {
               <div className="af-class-flex-container w-container">
                 <div className="af-class-div-block-6">
                   <h1 className="af-class-heading">Collective<br />Entertainment</h1>
-                  <p className="af-class-paragraph">Our best work happens when we're all rowing together toward the most significant outcomes.<br /><br />We help you get a handle on Free-Top-Play (F2P) product development each step of the way.<br /><br />Formalize your process, improve your metrics, and scale your producs.<br /><br />15+ years developing games, building infrastructure, managing products, and guiding teams.<br /></p>
+                  <p className="af-class-paragraph">Our best work happens when we're all rowing together toward the most significant outcomes.<br /><br />We help you get a handle on Free-Top-Play (F2P) mobile game development each step of the way.<br /><br />Formalize your process, improve your metrics, and scale your user acquisition.<br /><br />15+ years developing games, building infrastructure, managing products, and guiding teams.<br /></p>
                 </div>
                 <div className="af-class-hero-image-mask"><img src="images/CELogo_Large_black.png" sizes="(max-width: 479px) 92vw, (max-width: 991px) 95vw, 450px" srcSet="images/CELogo_Large_black-p-500.png 500w, images/CELogo_Large_black-p-800.png 800w, images/CELogo_Large_black-p-1080.png 1080w, images/CELogo_Large_black-p-1600.png 1600w, images/CELogo_Large_black-p-2000.png 2000w, images/CELogo_Large_black-p-2600.png 2600w, images/CELogo_Large_black-p-3200.png 3200w, images/CELogo_Large_black.png 5000w" alt className="af-class-hero-image" /></div>
               </div>
@@ -73,18 +74,7 @@ class IndexView extends React.Component {
                 <div className="af-class-centered-container w-container">
                   <h2 className="af-class-heading-3">The Framework</h2>
                   <p className="af-class-paragraph-3">A F2P mobile game framework that formalizes best practices, from initial market research, to scaling your publishing.<br />Coming Soon.</p>
-                  <div className="af-class-form-block w-form">
-                    <form id="email-form" name="email-form" data-name="Email Form" className="af-class-subscribe-form-flex">
-                      <div className="af-class-subscribe-form-input-wrapper"><input type="email" className="af-class-subscribe-form-input w-input" maxLength={256} name="Subscriber-Email" data-name="Subscriber Email" placeholder="E-Mail Address" id="Subscriber-Email" required /></div>
-                      <div className="af-class-div-block-5"><input type="submit" defaultValue="Subscribe" data-wait="Please wait..." className="af-class-submit-button-2 w-button" /></div>
-                    </form>
-                    <div className="af-class-success-message w-form-done">
-                      <div className="af-class-text-block">Thank you for subscribing.</div>
-                    </div>
-                    <div className="af-class-error-message af-class-error-message-2 w-form-fail">
-                      <div className="af-class-text-block-2">Subscription&nbsp;Error.&nbsp;Please try again.</div>
-                    </div>
-                  </div>
+                  <FrameworkFormView.Controller-af-sock-framework-form />
                 </div>
               </div>
               <div className="af-class-column-2 w-col w-col-6">
